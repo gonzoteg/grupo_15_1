211ce87fdb14202772642af6660540630b1d02ac
Gonzalo <gonzoteg@gmail.com>
2024-12-07 22:36:16 +0000

    Actualizacion

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, HTTPException
-from app.models.top_ctr_model import TopCTRModel          # Importar la clase TopCTRModel
-from app.models.top_product_model import TopProductModel
-from app.testdb import get_db_connection
 import psycopg2
+
+# Conexión a la base de datos
+def get_db_connection():
+    try:
+        conn = psycopg2.connect(
+            host="grupo-15-rds.cf4i6e6cwv74.us-east-1.rds.amazonaws.com",
+            database="postgres",
+            user="postgres",
+            password="nvmQVqg3d46tCJa",
+            port=5432
+            
+        )
+        return conn
+    except Exception as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
 
 app = FastAPI(title="AdTech Recommendation API")
 
@@ -12,21 +25,25 @@ async def get_recommendations(advertiser: str, model: str):
         conn = get_db_connection()
         cur = conn.cursor()
         
-        if model == "TopProduct":
+        if model == "TopProducts":
             cur.execute("""
-                SELECT product_id, views FROM TopProduct WHERE advertiser_id = %s;
-            """, (advertiser,))
+                SELECT product_id, views FROM top_products WHERE advertiser_id = %s AND date = (
+                SELECT MAX(date) FROM top_products WHERE advertiser_id = %s) ORDER BY views DESC 
+                LIMIT 1;
+            """, (advertiser, advertiser))
             recommendations = cur.fetchall()
             # Formatear la respuesta
             recommendations = [{"product_id": row[0], "views": row[1]} for row in recommendations]
         
         elif model == "TopCTR":
             cur.execute("""
-                SELECT product_id, clicks, impressions, ctr FROM TopCTR WHERE advertiser_id = %s;
-            """, (advertiser,))
+                SELECT product_id, ctr FROM top_ctr_products WHERE advertiser_id = %s AND date = (
+                SELECT MAX(date) FROM top_ctr_products WHERE advertiser_id = %s) ORDER BY ctr DESC 
+                LIMIT 1;
+            """, (advertiser, advertiser))
             recommendations = cur.fetchall()
             # Formatear la respuesta
-            recommendations = [{"product_id": row[0], "clicks": row[1], "impressions": row[2], "ctr": row[3]} for row in recommendations]
+            recommendations = [{"product_id": row[0], "ctr": row[1]} for row in recommendations]
         
         else:
             raise HTTPException(status_code=400, detail="Invalid model specified")
@@ -49,20 +66,38 @@ async def get_history(advertiser: str):
     try:
         # Obtener el historial de TopProduct
         cur.execute("""
-            SELECT product_id, views FROM TopProduct WHERE advertiser_id = %s;
-        """, (advertiser,))
+            WITH RankedProducts AS (
+            SELECT product_id, views, date,
+            ROW_NUMBER() OVER (PARTITION BY date ORDER BY views DESC) AS rank
+            FROM top_products WHERE advertiser_id = %s AND date >= 
+            ( SELECT MAX(date) - INTERVAL '6 days' 
+            FROM top_products WHERE advertiser_id = %s) 
+            )
+            SELECT product_id, views, date
+            FROM RankedProducts WHERE rank = 1
+            ORDER BY date DESC;
+        """, (advertiser, advertiser))
         top_product_history = cur.fetchall()
         
         # Obtener el historial de TopCTR
         cur.execute("""
-            SELECT product_id, clicks, impressions, ctr FROM TopCTR WHERE advertiser_id = %s;
-        """, (advertiser,))
+            WITH RankedProducts AS (
+            SELECT product_id, ctr, date,
+            ROW_NUMBER() OVER (PARTITION BY date ORDER BY ctr DESC) AS rank
+            FROM top_ctr_products WHERE advertiser_id = %s AND date >= (
+            SELECT MAX(date) - INTERVAL '6 days' 
+            FROM top_ctr_products WHERE advertiser_id = %s)
+            )
+            SELECT product_id, ctr, date
+            FROM RankedProducts WHERE rank = 1
+            ORDER BY date DESC;
+        """, (advertiser, advertiser))
         top_ctr_history = cur.fetchall()
 
         # Formatear la respuesta
         history = {
-            "top_products": [{"product_id": row[0], "views": row[1]} for row in top_product_history],
-            "top_ctr": [{"product_id": row[0], "clicks": row[1], "impressions": row[2], "ctr": row[3]} for row in top_ctr_history]
+            "top_products": [{"product_id": row[0], "views": row[1], "date": row[2]} for row in top_product_history],
+            "top_ctr": [{"product_id": row[0], "ctr": row[1], "date": row[2]} for row in top_ctr_history]
         }
 
         return {"history": history}
@@ -84,14 +119,8 @@ async def get_stats():
     try:
         # Contar anunciantes distintos y total de recomendaciones
         cur.execute("""
-            SELECT 
-                COUNT(DISTINCT advertiser_id) as advertiser_count_TopCTR,
-                (SELECT COUNT(*) FROM TopProduct) + (SELECT COUNT(*) FROM TopCTR) as total_recommendations
-            FROM TopProduct;
-            SELECT 
-                COUNT(DISTINCT advertiser_id) as advertiser_count_TopProduct,
-                (SELECT COUNT(*) FROM TopProduct) + (SELECT COUNT(*) FROM TopCTR)
-            FROM TopCTR;
+            SELECT COUNT(DISTINCT advertiser_id) as advertiser_count_TopProduct FROM top_products;
+            SELECT COUNT(DISTINCT advertiser_id) as advertiser_count_TopCTR FROM top_ctr_products;
         """)
         
         
@@ -99,7 +128,6 @@ async def get_stats():
         
         return {
             "advertiser_count": stats[0][0],
-            "total_recommendations": stats[0][1]
         }
     
     except Exception as e:
